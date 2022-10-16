788e2036d73e35ed176ca2cc84c2733530d70362
Petr Parolek <petr.parolek@gmail.com>
2022-10-16 20:54:17 +0200

    browserless test

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use Contributte\Application\Response\Fly\Adapter\StdoutAdapter;
+use Contributte\Application\Response\Fly\Buffer\Buffer;
+use Contributte\Application\Response\Fly\FlyFileResponse;
 use Contributte\PdfResponse\PdfResponse;
 use DateTime;
 use NAttreid\Invoices\Entities\Currency\CZK;
@@ -12,7 +15,10 @@ use NAttreid\Invoices\Entities\Item;
 use NAttreid\Invoices\Entities\Receiver;
 use NAttreid\Invoices\Entities\Supplier;
 use NAttreid\Invoices\PrintInvoice;
+use Nette\Application\Responses\FileResponse;
 use Nette\Application\UI\Presenter;
+use Nette\Http\IRequest;
+use Nette\Http\IResponse;
 use Nette\Localization\ITranslator;
 use Nette\Utils\ArrayHash;
 
@@ -42,12 +48,12 @@ final class HomepagePresenter extends Presenter
 		$receiver->city = 'Praha 2';
 
 		$supplier = new Supplier();
-		$supplier->ico = '53948874';
-		$supplier->dic = 'CZ53948874';
-		$supplier->name = 'Firma s.r.o.';
-		$supplier->street = 'U lesíka 123';
+		$supplier->ico = '28115708';
+		$supplier->dic = 'CZ28115708';
+		$supplier->name = 'WEDOS Internet, a.s.';
+		$supplier->street = 'Masarykova 1230';
 		$supplier->zip = '10200';
-		$supplier->city = 'Praha 2';
+		$supplier->city = '373 41 Hluboká nad Vltavou';
 		//$supplier->country = "Česká republika";
 		$supplier->text = "testovvací objednávka";
 		$supplier->bankAccount = '123/0100';
@@ -57,13 +63,13 @@ final class HomepagePresenter extends Presenter
 		$supplier->email = 'info@example.com';
 
 		$invoice = new Invoice();
-		$invoice->id = 2022090501;
-		$invoice->variable = 2022090501;
+		$invoice->id = 21774114;
+		$invoice->variable = 21774114;
 		$invoice->constant = 123;
-		$datetime = new DateTime();
-		$invoice->dateIssue = clone  $datetime;
-		$invoice->dueDate = $datetime->modify('+14 days');
-		$invoice->taxDate = $datetime;
+		$datetime = new DateTime('2022-07-01');
+		$invoice->dateIssue = clone $datetime;
+		$invoice->dueDate = clone $datetime;
+		$invoice->taxDate = clone $datetime;
 		$invoice->vat = true;
 		$invoice->text = 'testovací faktura';
 		$invoice->currency = new CZK();
@@ -381,8 +387,48 @@ final class HomepagePresenter extends Presenter
 		$printer->setInvoice($invoice);
 		$printer->setTemplateFile(__DIR__ . "/templates/invoice.latte");
 
-		$response = $printer->getResponse();
+		/*$response = $printer->getResponse();
+		$response->getMPDF()->SetFooter('{PAGENO} / {nb}'); // footer
 		$response->setSaveMode(PdfResponse::INLINE);
+
+		$this->sendResponse($response);*/
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, 'http://browserless:3000/pdf');
+		curl_setopt($ch, CURLOPT_HTTPHEADER, [
+			'Cache-Control: no-cache',
+			'Content-Type: application/json',
+		]);
+
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+			'html' => $printer->getTemplate()->renderToString(),
+			'options' => [
+				'displayHeaderFooter' => false,
+				'printBackground' => true,
+				'format' => 'A4',
+				'margin' => [
+					'bottom' => '2.5cm',
+					'left' => '2.5cm',
+					'right' => '2.5cm',
+					'top' => '2.5cm',
+				],
+			],
+		]));
+
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 600);
+
+		$result = curl_exec($ch);
+
+		curl_close($ch);
+
+		$adapter = new StdoutAdapter(function (Buffer $buffer, IRequest $request, IResponse $response) use ($result): void {
+			$response->setHeader('Content-Type', 'application/pdf');
+
+			$buffer->write($result);
+		});
+		$response = new FlyFileResponse($adapter, 'invoice.pdf');
 
 		$this->sendResponse($response);
 	}
